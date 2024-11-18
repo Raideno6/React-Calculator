cc54c64365ea96297af1ebbdd2f42ca18ce273df
SOSHrspitz <167810027+SOSHrspitz@users.noreply.github.com>
2024-11-18 15:06:01 -0500

    Calculating equation and displays answer to console
    Calculator displays output to console.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,11 +9,11 @@ export default class Calculator extends Component {
     render() {
         return (
             <div className="grid">
-                <input type="text" id="calculatorField" inputMode="numeric" className="calculatorDisplay"/>
-                {[7, 8, 9, "÷", 4, 5, 6, "x", 1, 2, 3, "-" ,".", 0, "=", "+"].map((i) => this.renderCalculatorButton(i))}
+                <input type="text" id="calculatorField" value={calculatorInput} inputMode="numeric" className="calculatorDisplay" readOnly/>
+                {["DEL", "AC", 7, 8, 9, "÷", 4, 5, 6, "x", 1, 2, 3, "-" ,".", 0, "=", "+"].map((i) => this.renderCalculatorButton(i))}
             </div>
         );
     } 
   }
 
-  
+  var calculatorInput = "";
--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -9,14 +9,55 @@ export default class CalculatorButton extends Component {
   }
     render() {
       return (
-        <button ref={this.buttonRef} className='calculatorButton' onClick={e => logNumber(this.props.value)}>
+        <button ref={this.buttonRef} key={this.props.value} className='calculatorButton' onClick={e => calculate(this.props.value)}>
           {this.props.value}
         </button>
       )
     }
   }
 
-  const logNumber = (buttonValue) => {
-    console.log(buttonValue)
+  var inputString = "";
+  const calculate = (buttonValue) => {
+    switch (buttonValue){
+      case "x": 
+        buttonValue = "*";
+        break;
+      case "÷": 
+      buttonValue = "/";
+      break;
+    }
+    if (buttonValue !== "DEL" && buttonValue !== "AC" && buttonValue !== "=") {
+      inputString.toString();
+      inputString += buttonValue.toString();
+      console.log(inputString)
+    }
+    else if (buttonValue === "DEL"){
+      try {
+      inputString = inputString.substring(0, inputString.length - 1);
+      console.log(inputString)
+      }
+      catch{
+        inputString = "";
+      }
+    }
+    else if (buttonValue === "AC"){
+      inputString = "";
+      console.log(inputString)
+    }
+    else if (buttonValue === "="){
+      try {
+      var ans = eval(inputString)
+
+      console.log("The answer is: " + ans)
+      inputString = ans;
+      if (inputString.toString() === "undefined"){
+        inputString = "";
+      }
+      }
+      catch{
+        console.log("Answer could not be found")
+        inputString = "";
+      }
+    }
   }
   
